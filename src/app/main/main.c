b7b3e1e003c4e73361ccda8d64112c29597504c4
Jonah Sprandel <jjsprandel@gmail.com>
2024-12-03 15:58:40 -0500

    wifi functionality added in source code github actions updated to test docker image builds and upload esp32 build output

--- src/app/main/main.c
+++ src/app/main/main.c
@@ -8,6 +8,7 @@
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+#include "main.h"
 
 
 
@@ -45,12 +46,14 @@ void app_main(void)
     /* Configure the peripheral according to the LED type */
     configure_led();
 
+    wifi_init();
+
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
         blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
     }
 }
 
